f94333c4b2511c3d81a5603e89e0e5da199d56dc
Dung Hoang <dung14000@gmail.com>
2021-04-20 15:36:25 +0700

    Update config

--- partner-login/db_config.php
+++ partner-login/db_config.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "localhost"; // Giữ mặc định là localhost
 $db_username    = "root"; //Can thay doi theo username truy cap MySQL
-$db_password    = "root";//Can thay doi theo mat khau truy cap MySQL
-$db_name    = "mnbcc066_partner";// Can thay doi theo ten ban tao cho database
+$db_password    = "gMGdFP22fN4bmsn";//Can thay doi theo mat khau truy cap MySQL
+$db_name    = "partner";// Can thay doi theo ten ban tao cho database
 
 if(function_exists('mysql_connect')) {
     $conn=mysql_connect("localhost", $db_username, $db_password) or die("Không thể kết nối đến MySQL server");
