f4ded4498ba4a217ac4215178ab2b34b849b6b85
Dung Hoang <dung14000@gmail.com>
2021-04-20 15:33:31 +0700

    Update config

--- partner-login/db_config.php
+++ partner-login/db_config.php
@@ -1,10 +1,14 @@
 <?php
 $db_host = "localhost"; // Giữ mặc định là localhost
 $db_username    = "root"; //Can thay doi theo username truy cap MySQL
-$db_password    = "gMGdFP22fN4bmsn";//Can thay doi theo mat khau truy cap MySQL
-$db_name    = "partner";// Can thay doi theo ten ban tao cho database
+$db_password    = "root";//Can thay doi theo mat khau truy cap MySQL
+$db_name    = "mnbcc066_partner";// Can thay doi theo ten ban tao cho database
 
-$conn=mysql_connect("localhost", $db_username, $db_password) or die("Không thể kết nối đến MySQL server");
+if(function_exists('mysql_connect')) {
+    $conn=mysql_connect("localhost", $db_username, $db_password) or die("Không thể kết nối đến MySQL server");
+}else {
+    $conn=mysqli_connect("localhost", $db_username, $db_password) or die("Không thể kết nối đến MySQL server");
+}
 
 $result=mysql_select_db($db_name) or die("Database không tồn tại. Có thể database chưa được tạo!");
 
